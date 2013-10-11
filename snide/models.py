107b139d9d1bef6ce303f8a3572106b66b5582c6
Michael Joseph <michaeljoseph+github@gmail.com>
2013-10-11 12:46:57 +0200

    Parse and test slide notes and content

--- snide/models.py
+++ snide/models.py
@@ -1,3 +1,5 @@
+import re
+
 import markdown2
 
 
@@ -7,9 +9,9 @@ class Deck(object):
         self.slides = []
         self.title = title
         self.text = text
-        print(text.split('---'))
+
         for slide in text.split('---'):
-            self.slides.append(Slide(slide))
+            self.slides.append(Slide(slide.lstrip()))
 
     @property
     def json(self):
@@ -20,26 +22,57 @@ class Deck(object):
 
 
 class Slide(object):
+    text = []
+    config = {}
+    slide = []
+    notes = []
 
     def __init__(self, text):
-        self.markdown = text
-        self.config = self.parse_config(text)
+        self.text = text.split('\n')
+        self.parse_slide()
 
     @property
     def html(self):
-        return markdown2.markdown(self.markdown)
+        if self.slide:
+            return markdown2.markdown('\n'.join(self.slide))
+        else:
+            return self.slide
 
-    def parse_config(self, slide):
-        # re of lines: key:value, key=value
-        return {
+    @property
+    def notes_html(self):
+        if self.notes:
+            return markdown2.markdown('\n'.join(self.notes))
+        else:
+            return self.notes
 
-        }
+    def parse_slide(self):
+        # config
+        pattern = re.compile("([a-z]+)\s*:\s*([a-z]+)")
+        text = []
+        for line in self.text:
+            if pattern.match(line):
+                self.config.update(
+                    dict(pattern.findall(line))
+                ) 
+            else:
+                text.append(line)
+
+        # slide and notes
+        self.text = text
+        if self.text.count('???'):
+            position = self.text.index('???')
+            self.slide, self.notes = self.text[0 : position-1], self.text[position + 1 : len(self.text)]
+        else:
+            self.slide = self.text
 
     @property
     def json(self):
         return {
-            'text': self.markdown,
-            'html': self.html,
             'config': self.config,
+            'text': self.text,
+            'slide': self.slide,
+            'html': self.html,
+            'notes': self.notes,
+            'notes_html': self.notes_html,
         }
 
--- tests/test_snide.py
+++ tests/test_snide.py
@@ -11,6 +11,7 @@ class TestDeck(BaseTestCase):
                           '# second slide').strip()
 
         self.expected_deck = Deck('foo', self.deck_text)
+
         self.expected_deck.slides = [
             Slide('# first slide'),
             Slide('# second slide')
@@ -26,19 +27,40 @@ class TestDeck(BaseTestCase):
 class TestSlide(BaseTestCase):
 
     def setUp(self):
+        self.slide_text = '# first slide\n\n- and\n- then\n- this'
         self.expected_slide = Slide('# first slide\n\n- and\n- then\n- this')
 
-    def test_slide(self):
+        self.notes_slide_text = ('# first point\n'
+                                 '# second point\n'
+                                 '???\n'
+                                 '# some notes\n').strip()
 
-        slide_text = '# first slide\n\n- and\n- then\n- this'
+        self.config_slide_text = ('class:inverse\n'
+                                  'transition:down\n'
+                                  '# first point\n'
+                                  '# second point\n').strip()
+
+    def test_slide(self):
         self.assertEquals(
             self.expected_slide.json,
             Slide(self.slide_text).json
         )
 
-        pass
+    def test_slide_config(self):
+        self.assertEquals(
+            {'class': 'inverse', 'transition': 'down'},
+            Slide(self.config_slide_text).config,
+        )
 
-    def test_slide_render(self):
-        pass
+    def test_slide_config_removed_from_text(self):
+        self.assertEquals(
+            ['# first point', '# second point'],
+            Slide(self.config_slide_text).text,
+        )
 
+    def test_slide_notes(self):
+        self.assertEquals(
+            ['# some notes'],
+            Slide(self.notes_slide_text).notes,
+        )
 
